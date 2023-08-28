40e8d638f6e5a81816530e68d6285a0ce62cdd7c
fenilsheladiya33 <fenilsheladiya3333@gmail.com>
2023-08-28 17:08:17 +0530

    new

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -2,34 +2,34 @@ const gituser = require('../model/usermodel');
 
 
 //insert
-exports.add_user = async (req,res) => {
-    try {
-        var data = await gituser.create(req.body);
 
-        res.status(200).json({
-            status:"insert succesful",
-            data
-        })
-    } catch (error) {
-        res.status(200).json({
-            error
-        })
-    }
+
+exports.user_add = async (req,res) => {
+
+    var data = await gituser.create(req.body);
+
+    res.status(200).json({
+        status:"insert successfully",
+        data
+    })
 }
+
+
+
 
 //show all
-exports.show_user = async (req,res) => {
+// exports.show_user = async (req,res) => {
 
-    try {
-        var data = await gituser.find();
+//     try {
+//         var data = await gituser.find();
 
-        res.status(200).json({
-            status:"user show succesful",
-            data
-        })
-    } catch (error) {
-        res.status(200).json({
-            error
-        })
-    }
-}
+//         res.status(200).json({
+//             status:"user show succesful",
+//             data
+//         })
+//     } catch (error) {
+//         res.status(200).json({
+//             error
+//         })
+//     }
+// }
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@
 var express = require('express');
 var router = express.Router();
 
-const{add_user,show_user} = require('../controller/usercontroller');
 
-router.post('/adduser',add_user);
-router.get('/getuser',show_user);
+const{user_add} = require('../controller/usercontroller');
+
+
+router.post('/add',user_add);
 
 module.exports = router;
